df464daf9e87a783c2de0e972a444e0098faedb5
Ben Brostoff <ben.brostoff@gmail.com>
2014-08-18 23:47:33 -0400

    Added boolean for drafted status

--- the_dropoff/app/controllers/dropoffs_controller.rb
+++ the_dropoff/app/controllers/dropoffs_controller.rb
@@ -1,12 +1,16 @@
 class DropoffsController < ApplicationController
 
   def index
-    @rbs = Player.where("position = 'RB'").order("projection DESC, dropoff DESC").take(10)
-    @qbs = Player.where("position = 'QB'").order("projection DESC, dropoff DESC").take(10) 
-    @wrs = Player.where("position = 'WR'").order("projection DESC, dropoff DESC").take(10)
-    @tes = Player.where("position = 'TE'").order("projection DESC, dropoff DESC").take(10)
+    @rbs = Player.where("position = 'RB', drafted: false").order("projection DESC, dropoff DESC").take(10)
+    @qbs = Player.where("position = 'QB', drafted: false").order("projection DESC, dropoff DESC").take(10) 
+    @wrs = Player.where("position = 'WR', drafted: false").order("projection DESC, dropoff DESC").take(10)
+    @tes = Player.where("position = 'TE', drafted: false").order("projection DESC, dropoff DESC").take(10)
 
     @pos_array = [@rbs, @qbs, @wrs, @tes]
+  end
+
+  def reset
+    Player.all.each { |player| player.drafted = false }
   end
 
   def offense
--- the_dropoff/db/seeds.rb
+++ the_dropoff/db/seeds.rb
@@ -3,9 +3,10 @@ require 'CSV'
 fantasy_data = CSV.read((File.join(Rails.root, 'db', 'season_14_project.csv')))
 
 fantasy_data.each do |player|
+  # excludes K and DEF
   if player[1] == "QB" || player[1] == "RB" || player[1] == "TE" || player[1] == "WR"
     Player.create(position: player[1], name: "#{player[3]} #{player[2]}", 
-                team: player[4], projection: player[-1].to_i)
+                team: player[4], projection: player[-1].to_i, drafted: false)
   end
 end
 
